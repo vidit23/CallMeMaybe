9ad31fd8d11d89fa479e0377a0dcb397e495bad4
vidit23 <viditvineet@hotmail.com>
2018-06-10 04:22:37 +0530

    rating system

--- server/serverhelper.js
+++ server/serverhelper.js
@@ -1,10 +1,33 @@
 const userDAO = require('../model/usersDAO');
 const tagDAO = require('../model/tagsDAO');
+const ratingDAO = require('../model/ratingsDAO');
+
+function getUserHigherRating(giver, receiver, whoCalled) {
+    return new Promise((resolve, reject) => {
+        let num;
+        userDAO.getUserByUserName(giver).then(result => {
+            num = result.ranking;
+            return userDAO.getUserByUserName(receiver);
+        }).then(doc => {
+            if (num > doc.ranking) {
+                return resolve(giver);
+            } else if (num < doc.ranking) {
+                return resolve(receiver);
+            } else {
+                return resolve(whoCalled);
+            }
+        }).catch(err => {
+            console.log('getUserHigherRating ', err);
+            return reject(err);
+        });
+    });
+}
+
 
 module.exports = {
-    getUser: function (id) {
+    getUser: function (username) {
         return new Promise((resolve, reject) => {
-            userDAO.getUserById(id).then(doc => {
+            userDAO.getUserByUserName(username).then(doc => {
                 return resolve(doc);
             }).catch(err => {
                 return reject(err);
@@ -56,7 +79,58 @@ module.exports = {
                 return resolve(doc);
             }).catch(err => {
                 return reject(err);
-            })
+            });
+        });
+    },
+    matchIsHelp: function (giver, receiver, whoCalled, isHelp) {
+        return new Promise((resolve, reject) => {
+            console.log('match isHelp ', receiver);
+            let exists = false, isMatch = false, daoIsHelp;
+            return ratingDAO.getRatingLatest(receiver, giver, whoCalled).then(result => {
+                if(result.length === 0) {
+                    // If no previous record exist in ratingsDB, add to it
+                    console.log('If no previous record exist in ratingsDB, add to it ');
+                    return ratingDAO.updateRating(giver, receiver, whoCalled, isHelp);
+                } else {
+                    // If previous record exists, store it in memory and delete it from DB
+                    exists = true;
+                    daoIsHelp = result.isHelp;
+                    console.log('If previous record exists, store it in memory and delete it from DB ');
+                    return ratingDAO.removeRating(result._id);
+                }
+            }).then(() => {
+                if (exists === true) {
+                    if (daoIsHelp === isHelp) {
+                        // add to both their ratings
+                        console.log('add to both their ratings ');
+                        let promiseArr = [userDAO.addToUserRanking(giver, 1), userDAO.addToUserRanking(receiver, 1)];
+                        return Promise.all(promiseArr);
+                    } else {
+                        // find on with higher rating, reduce the other ones rating
+                        console.log('find on with higher rating, reduce the other ones rating');
+                        return getUserHigherRating(giver, receiver, whoCalled);
+                    }
+                } else {
+                    // Nothing to do, return empty
+                    return;
+                }
+            }).then(doc => {
+                if (typeof doc === 'string' && doc === giver) {
+                    console.log('giver is greater ');
+                    let promiseArr = [userDAO.addToUserRanking(giver, 1), userDAO.addToUserRanking(receiver, -1)];
+                    return Promise.all(promiseArr);
+                } else if (typeof doc === 'string' && doc === receiver) {
+                    console.log('receiver is greater ');
+                    let promiseArr = [userDAO.addToUserRanking(giver, -1), userDAO.addToUserRanking(receiver, 1)];
+                    return Promise.all(promiseArr);
+                } else {
+                    return;
+                }
+            }).then(() => {
+                return resolve();
+            }).catch(err => {
+                return reject(err);
+            });
         });
     }
 }

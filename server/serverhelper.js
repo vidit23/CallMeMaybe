700868f48df657164526ef29511c8fe0c4bb314f
balab23 <balab23@gmail.com>
2018-06-09 23:07:12 +0530

    getuserbytag

--- server/serverhelper.js
+++ server/serverhelper.js
@@ -1,13 +1,24 @@
 const userDAO = require('../model/usersDAO');
 
 module.exports = {
-    getUser: function() {
+    getUser: function(id) {
         return new Promise((resolve, reject) => {
-            userDAO.getUserById('5b1b780f0045134e103e8c03').then(doc => {
+            userDAO.getUserById(id).then(doc => {
+                return resolve(doc);
+            }).catch(err => {
+                return reject(err);
+            })
+        })
+    },
+    getUserGivenTag: function(tag) {
+        return new Promise((resolve, reject) => {
+            userDAO.getUserByTag(tag).then(doc => {
                 return resolve(doc);
             }).catch(err => {
                 return reject(err);
             })
         })
     }
+
+
 }
